4b884a194d90f16e7e58bd8709477052c6f32267
Roberto Deresu <ioan-roberto.deresu@my.fmi.unibuc.ro>
2018-07-17 19:10:04 +0300

    Add project files.

---
+++ InstaMonitor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InstaMonitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
